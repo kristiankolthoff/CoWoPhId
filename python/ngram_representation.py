99428c83fff9b6e9168079a3539509002cdae651
kkolthof <k_kolthoff@web.de>
2018-07-30 11:35:42 +0200

    Updates and bugfixes of MWE representation methods

--- python/ngram_representation.py
+++ python/ngram_representation.py
@@ -8,16 +8,45 @@ def missing_strat_random(word, dim):
 def missing_strat_zeros(word, dim):
     return np.zeros(dim)
 
-def ngram_repr_bag_of_words(word_vecs):
+def ngram_repr_bow_mean(word_vecs, *args):
     return np.mean(word_vecs, axis = 0)
 
-def ngram_repr_weighted_bag_of_word(word_vecs, words, alpha, unigram_prob):
-    weighted_vecs = [(alpha/(alpha+unigram_prob.get(word))) \
+def ngram_repr_bow_median(word_vecs, *args):
+    return np.median(word_vecs, axis = 0)
+
+def ngram_repr_bow_max(word_vecs, *args):
+    return np.max(word_vecs, axis = 0)
+
+def ngram_repr_bow_min(word_vecs, *args):
+    return np.min(word_vecs, axis = 0)
+
+def ngram_repr_bow_concat(word_vecs, ngram_bow = [ngram_repr_bow_max]):
+    return np.concatenate([ngram_repr(word_vecs) for ngram_repr in ngram_bow])
+
+def ngram_repr_weighted_bow(word_vecs, words, alpha, unigram_prob, ngram_repr):
+    weighted_vecs = [(alpha/(alpha+unigram_prob(word))) * vec \
                 for vec, word in zip(word_vecs, words)]
-    matrix = np.array(weighted_vecs)
-    u, s, vh = np.linalg.svd(matrix, full_matrices=True)
-    vec_mean = np.mean(weighted_vecs, axis = 0)
-    singular_vec = u[:0]
-    vec_final = vec_mean - (singular_vec * \
-                        np.transpose(singular_vec) * vec_mean)
-    return vec_final
+    return ngram_repr(weighted_vecs)  
+
+#word_freq_wiki = {}
+#sum_counts = 0
+#with open("resources/word-freq-dumps/enwiki-20150602-words-frequency.txt", encoding="utf8") as file:
+#    for line in file:
+#        word, freq = line.partition(" ")[::2]
+#        sum_counts+=int(freq)
+#        word_freq_wiki[word.strip()] = int(freq)
+#        
+#def get_unigram_probability(word):
+#    return word_freq_wiki[word] / sum_counts
+        
+
+#from gensim.test.utils import datapath, get_tmpfile
+#from gensim.models import KeyedVectors
+#from gensim.scripts.glove2word2vec import glove2word2vec
+#
+#MAIN_PATH = 'D:/workspace_python/CoWoReId/python/resources/word-embeddings/'
+#glove_file = datapath(MAIN_PATH + 'glove.6B.200d.txt')
+#tmp_file = get_tmpfile("glove2word2vec.txt")
+#glove2word2vec(glove_file, tmp_file)
+#
+#model_glove = KeyedVectors.load_word2vec_format(tmp_file)
